7f45beeac06e390160cd9038592631417be3041b
Matheus Girardin <7042521-mgirardin.inoa@users.noreply.gitlab.com>
2021-07-31 17:41:48 -0300

    feat(tokens): TokenEnum

--- core/Program.cs
+++ core/Program.cs
@@ -1,9 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using SimpleScriptLanguageCompiler.Common;
 using SimpleScriptLanguageCompiler.LexicalAnalyzer;
 
 var fileContent = File.ReadAllText(args[0]);
 LexicalAnalyzer.Run(fileContent)
-    .ForEach(token => {
-        System.Console.WriteLine(token);
-    });
+    .ForEach(Console.WriteLine);
