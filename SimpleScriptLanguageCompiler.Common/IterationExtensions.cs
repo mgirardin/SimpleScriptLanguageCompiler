a3fa9d4a822e115b5891820dcf41f24f19afa974
Matheus Girardin <7042521-mgirardin.inoa@users.noreply.gitlab.com>
2021-07-31 17:17:25 -0300

    feat(common): IterationExtensions

---
+++ SimpleScriptLanguageCompiler.Common/IterationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleScriptLanguageCompiler.Common {
+    public static class IterationExtensions {
+        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action) {
+            foreach (var item in items) action(item);
+        }
+
+        public static void ForEachTry<T>(this IEnumerable<T> items,
+            Action<T> action,
+            Action<T, Exception> handleExceptions = null,
+            Action<T> handleFinally = null) {
+            var currentItem = default(T);
+            try {
+                items.ForEach(i => {
+                    currentItem = i;
+                    try {
+                        action(i);
+                    } catch (Exception e) {
+                        handleExceptions?.Invoke(i, e);
+                    } finally {
+                        handleFinally?.Invoke(i);
+                    }
+                });
+            } catch (Exception e) {
+                handleExceptions?.Invoke(currentItem, e);
+            }
+        }
+    }
+}
--- core/Program.cs
+++ core/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System.IO;
+using SimpleScriptLanguageCompiler.Common;
 using SimpleScriptLanguageCompiler.LexicalAnalyzer;
 
 var fileContent = File.ReadAllText(args[0]);
-//TODO: Create .ForEach extension method in new project SimpleScriptLanguageCompiler.Common
-var tokens = LexicalAnalyzer.Run(fileContent);
-foreach (var token in tokens) {
-    System.Console.WriteLine(token);
-}
+LexicalAnalyzer.Run(fileContent)
+    .ForEach(token => {
+        System.Console.WriteLine(token);
+    });
