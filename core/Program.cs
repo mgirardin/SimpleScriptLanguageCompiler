f5ab0586a359789a7b4511cc90fbff093cb456bf
Matheus Girardin <7042521-mgirardin.inoa@users.noreply.gitlab.com>
2021-07-31 16:29:56 -0300

    refactor(core): Program.cs

--- core/Program.cs
+++ core/Program.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.IO;
 
-namespace SimpleScriptLanguageCompiler.Core {
-    class Program {
-        static void Main(string[] args) {
-            var fileContent = File.ReadAllText(args[0]);
-            Console.WriteLine(fileContent);
-        }
-    }
-}
+var fileContent = File.ReadAllText(args[0]);
+Console.WriteLine(fileContent);
